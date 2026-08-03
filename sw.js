97e098103d02c8be40b8750486f80c6499c71f2c
shadrachgideon20200-web <shadrachgideon20200@gmail.com>
2026-08-03 19:19:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,13 @@
-const CACHE='amasan-v10';
-self.addEventListener('install', e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/index.html','/track.html','/manifest.json'])));
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
 });
-self.addEventListener('fetch', e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
 });
